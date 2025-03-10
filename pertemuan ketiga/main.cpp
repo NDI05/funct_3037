a91d58fa708258bb4997508a9c305fb5f8fb5892
NDI05 <andhikap877@gmail.com>
2025-03-10 10:16:06 +0700

    init

--- pertemuan ketiga/main.cpp
+++ pertemuan ketiga/main.cpp
@@ -1,14 +1,22 @@
-//
-//  main.cpp
-//  pertemuan ketiga
-//
-//  Created by NDI on 10/03/25.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+float calculateAverange(int x, int y){
+    return (x+y)/2;
+}
+
+string givingStatus(int x){
+    if(x >= 75)
+        return "Lulus";
+    else
+        return "gagal";
+}
+
+int main () {
+    int nilai1, nilai2;
+    cout << "Silahkan Masukkan Nilai Pertama Anda =";
+    cin >> nilai1;
+    cout << "Silahkan Masukkan Nilai Kedua Anda =";
+    cin >> nilai2;
+    cout << "Status Kelulusan Anda :" << givingStatus(calculateAverange(nilai1, nilai2)) << endl;
 }
